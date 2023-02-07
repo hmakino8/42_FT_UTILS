b5983273228714311c18f201bd95bf25e2fa74e7
Hiroaki Makino <8makino.42tokyo@gmail.com>
2023-02-08 03:07:40 +0900

    delete ft_printf2

--- src/stdio/ft_printf2/test.sh
+++
@@ -1,8 +0,0 @@
-make
-gcc libftprintf.a main.c ../get_next_line/*.c -o printf
-gcc libftprintf.a main.c ../get_next_line/*.c -D FT_PRINTF -o ft_printf
-./printf > printf.txt
-./ft_printf > ft_printf.txt
-diff -a --suppress-common-lines printf.txt ft_printf.txt
-rm -rf printf ft_printf printf.txt ft_printf.txt
-make fclean
--- src/stdio/ft_printf2/test2.sh
+++
@@ -1,2 +0,0 @@
-gcc *.c -I ../../../include/ ../../string/*.c ../get_next_line/*.c ../../stdlib/*.c ../../ctype/*.c ../../linkedlist/*.c ../*.c -D FT_PRINTF; ./a.out > ft.txt; gcc *.c -I ../../../include/ ../../string/*.c ../get_next_line/*.c ../../stdlib/*.c ../../ctype/*.c ../../linkedlist/*.c ../*.c;./a.out > original.txt;diff -a --suppress-common-lines ft.txt original.txt; rm ft.txt original.txt
-
